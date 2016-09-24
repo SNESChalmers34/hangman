efdd8bcfbe63f1349d582d263a73312ef1a4a80b
Mark Sweet <marks828@gmail.com>
2016-09-24 10:06:46 -0400

    cycle through alphabet

---
+++ script.js
@@ -0,0 +1,24 @@
+//creat start function
+function start(){
+  //array of alphabet
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  //cycle through array
+  function cycle(){
+    for (i = 0; i < alphabet.length; i++)
+    console.log(alphabet[i])
+  }
+  cycle()
+
+//end of start function
+}
+
+
+
+
+//create div for alphabet
+//
+
+//start function
+start()
+//rerun start function
