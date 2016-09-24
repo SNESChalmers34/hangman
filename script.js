73d0c62160dd01588f52ad330b6cad41862da742
Mark Sweet <marks828@gmail.com>
2016-09-24 17:45:07 -0400

    appedning alphabet to divs

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
-//creat start function
-function start(){
-  //array of alphabet
-  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-// letter divs
-  // function letters(){
-    for (i = 0; i < 26; i++){
-    let div = $('div').addClass('letters')
-    $('body').append('<div></div')
-    }
-  // }
-
-//end of start function
-}
-
-
-
-
-//create div for alphabet
-//
-
-//start function
-start()
-//rerun start function
-
-
-// RANDOM CODE
-
-//cycle through array
-function cycle(){
-  for (i = 0; i < alphabet.length; i++)
-  console.log(alphabet[i])
-}
-cycle()
+$('#start').click(function(){
+  $(this).hide()
+  let alphabet = ['a', 'b']
+  for(i = 0; i < alphabet.length ; i++){
+    var div = $('<div></div>').addClass('square')
+    $('main').append(div.html(alphabet[i]))
+  }
+})
