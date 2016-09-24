4bc3b2e99f4100e9d8682cf4a18457492a842a27
Mark Sweet <marks828@gmail.com>
2016-09-24 10:23:23 -0400

    new branch

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ function start(){
   //array of alphabet
   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
   "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  //cycle through array
-  function cycle(){
-    for (i = 0; i < alphabet.length; i++)
-    console.log(alphabet[i])
-  }
-  cycle()
+
+// letter divs
+  // function letters(){
+    for (i = 0; i < 26; i++){
+    let div = $('div').addClass('letters')
+    $('body').append('<div></div')
+    }
+  // }
 
 //end of start function
 }
@@ -22,3 +24,13 @@ function start(){
 //start function
 start()
 //rerun start function
+
+
+// RANDOM CODE
+
+//cycle through array
+function cycle(){
+  for (i = 0; i < alphabet.length; i++)
+  console.log(alphabet[i])
+}
+cycle()
